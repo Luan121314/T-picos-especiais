14b8fa281ae7d54fe6ab888b03aaa5b0a34fcaff
Logon São Miguel <logon@cds.smg.lab>
2019-08-23 11:33:26 -0300

    App de calculadora com as 4 operações básicas para desktop desenvolvido em C#

---
+++ Calculadora simples/WindowsFormsApp2/Calculadora.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculadora));
+            this.Calcular = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajudaToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.res = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbSoma = new System.Windows.Forms.RadioButton();
+            this.rbSubtração = new System.Windows.Forms.RadioButton();
+            this.rbMultiplicação = new System.Windows.Forms.RadioButton();
+            this.rbDivisão = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Calcular
+            // 
+            this.Calcular.Location = new System.Drawing.Point(54, 301);
+            this.Calcular.Name = "Calcular";
+            this.Calcular.Size = new System.Drawing.Size(75, 23);
+            this.Calcular.TabIndex = 1;
+            this.Calcular.Text = "Calcular";
+            this.Calcular.UseVisualStyleBackColor = true;
+            this.Calcular.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.ajudaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(388, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.sairToolStripMenuItem.Text = "sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // ajudaToolStripMenuItem
+            // 
+            this.ajudaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajudaToolStripMenuItem1});
+            this.ajudaToolStripMenuItem.Name = "ajudaToolStripMenuItem";
+            this.ajudaToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.ajudaToolStripMenuItem.Text = "Sobre";
+            // 
+            // ajudaToolStripMenuItem1
+            // 
+            this.ajudaToolStripMenuItem1.Name = "ajudaToolStripMenuItem1";
+            this.ajudaToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.ajudaToolStripMenuItem1.Text = "Sobre";
+            this.ajudaToolStripMenuItem1.Click += new System.EventHandler(this.ajudaToolStripMenuItem1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton3,
+            this.toolStripButton4});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(388, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "Sair do programa";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "Informação";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Valor 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(78, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Valor 2";
+            // 
+            // res
+            // 
+            this.res.AutoSize = true;
+            this.res.Location = new System.Drawing.Point(178, 306);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(55, 13);
+            this.res.TabIndex = 6;
+            this.res.Text = "Resultado";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(167, 71);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(167, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbDivisão);
+            this.groupBox1.Controls.Add(this.rbMultiplicação);
+            this.groupBox1.Controls.Add(this.rbSubtração);
+            this.groupBox1.Controls.Add(this.rbSoma);
+            this.groupBox1.Location = new System.Drawing.Point(70, 154);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 125);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Selecione a operação";
+            // 
+            // rbSoma
+            // 
+            this.rbSoma.AutoSize = true;
+            this.rbSoma.Checked = true;
+            this.rbSoma.Location = new System.Drawing.Point(0, 20);
+            this.rbSoma.Name = "rbSoma";
+            this.rbSoma.Size = new System.Drawing.Size(52, 17);
+            this.rbSoma.TabIndex = 0;
+            this.rbSoma.TabStop = true;
+            this.rbSoma.Text = "Soma";
+            this.rbSoma.UseVisualStyleBackColor = true;
+            // 
+            // rbSubtração
+            // 
+            this.rbSubtração.AutoSize = true;
+            this.rbSubtração.Location = new System.Drawing.Point(0, 43);
+            this.rbSubtração.Name = "rbSubtração";
+            this.rbSubtração.Size = new System.Drawing.Size(74, 17);
+            this.rbSubtração.TabIndex = 1;
+            this.rbSubtração.TabStop = true;
+            this.rbSubtração.Text = "Subtração";
+            this.rbSubtração.UseVisualStyleBackColor = true;
+            // 
+            // rbMultiplicação
+            // 
+            this.rbMultiplicação.AutoSize = true;
+            this.rbMultiplicação.Location = new System.Drawing.Point(0, 67);
+            this.rbMultiplicação.Name = "rbMultiplicação";
+            this.rbMultiplicação.Size = new System.Drawing.Size(87, 17);
+            this.rbMultiplicação.TabIndex = 2;
+            this.rbMultiplicação.TabStop = true;
+            this.rbMultiplicação.Text = "Multiplicação";
+            this.rbMultiplicação.UseVisualStyleBackColor = true;
+            // 
+            // rbDivisão
+            // 
+            this.rbDivisão.AutoSize = true;
+            this.rbDivisão.Location = new System.Drawing.Point(0, 91);
+            this.rbDivisão.Name = "rbDivisão";
+            this.rbDivisão.Size = new System.Drawing.Size(60, 17);
+            this.rbDivisão.TabIndex = 3;
+            this.rbDivisão.TabStop = true;
+            this.rbDivisão.Text = "Divisão";
+            this.rbDivisão.UseVisualStyleBackColor = true;
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 369);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.Calcular);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Calculadora";
+            this.Text = "Calculadora simples";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Calcular;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripMenuItem ajudaToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label res;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbDivisão;
+        private System.Windows.Forms.RadioButton rbMultiplicação;
+        private System.Windows.Forms.RadioButton rbSubtração;
+        private System.Windows.Forms.RadioButton rbSoma;
+    }
+}
+
---
+++ Calculadora simples/WindowsFormsApp2/Calculadora.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Autor form2 = new Autor();
+            form2.ShowDialog();
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            Autor form2 = new Autor();
+            form2.ShowDialog();
+        }
+
+        private void ajudaToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Autor form2 = new Autor();
+            form2.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double n1, n2;
+            double result = 0.0;
+            n1 = Convert.ToDouble(textBox1.Text);
+            n2 = Convert.ToDouble(textBox2.Text);
+
+
+            if (rbSoma.Checked) result = n1 + n2;
+            if (rbSubtração.Checked) result = n1 - n2;
+            if (rbMultiplicação.Checked) result = n1 * n2;
+
+            res.Text = "Resultado: " + result.ToString();
+            if (rbDivisão.Checked)
+            {
+                if(n1 >= 1 && n2 >= 1)
+                {
+                    result = n1 / n2;
+                    res.Text = "Resultado: " + result.ToString();
+                }
+                else
+                {
+                    res.Text = "Operação não pode ser realizada";
+                }
+            }
+
+            
+        }
+    }
+}
